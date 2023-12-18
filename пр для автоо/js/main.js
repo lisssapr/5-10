11e16af69d2d44af431e52c246d0a8f144c0e4df
lisssapr <144418020+lisssapr@users.noreply.github.com>
2023-12-18 14:23:40 +0300

    Update main.js

--- пр для автоо/js/main.js
+++ пр для автоо/js/main.js
@@ -1,11 +1,10 @@
 "use strict"
 // // ЭКРАННАЯ КЛАВИАТУРА 1 и 2
 
-// const keyboard = document.getElementById('keyboard');
-// const inputField = document.getElementById('inputField');
+// let keyboard = document.getElementById('keyboard');
+// let inputField = document.getElementById('inputField');
 // let capsLockEnabled = false;
 
-// // Функция для добавления символа в поле ввода
 // function addToInput(value) {
 //     if (capsLockEnabled && /[a-zA-Z]/.test(value)) {
 //         value = value.toUpperCase();
@@ -13,41 +12,33 @@
 //     inputField.value += value;
 // }
 
-// // Создаем кнопки для каждой цифры
 // for (let i = 1; i <= 9; i++) {
-//     const button = document.createElement('button');
+//     let button = document.createElement('button');
 //     button.textContent = i;
 //     button.addEventListener('click', () => addToInput(i));
 //     keyboard.appendChild(button);
 // }
 
-// // Кнопка "0"
-// const zeroButton = document.createElement('button');
+// let zeroButton = document.createElement('button');
 // zeroButton.textContent = '0';
 // zeroButton.addEventListener('click', () => addToInput('0'));
 // keyboard.appendChild(zeroButton);
 
-// // Кнопки букв
-// const letters = 'abcdefghijklmnopqrstuvwxyz';
+// let letters = 'abcdefghijklmnopqrstuvwxyz';
 // for (const letter of letters) {
-//     const button = document.createElement('button');
+//     let button = document.createElement('button');
 //     button.textContent = letter;
 //     button.addEventListener('click', () => addToInput(letter));
 //     keyboard.appendChild(button);
 // }
 
-// // Кнопка Caps Lock
-// const capsLockButton = document.createElement('button');
+// let capsLockButton = document.createElement('button');
 // capsLockButton.textContent = 'Caps Lock';
 // capsLockButton.addEventListener('click', () => {
 //     capsLockEnabled = !capsLockEnabled;
 //     capsLockButton.style.backgroundColor = capsLockEnabled ? 'lightblue' : '';
 // });
 // keyboard.appendChild(capsLockButton);
-
-
-
-
 
 
 
@@ -63,10 +54,8 @@
 // function renderCalendar() {
 //     let daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
-//   // Clear previous content
 //   numbersList.innerHTML = '';
 
-//   // Set month and year above the calendar
 //   monthYearElement.textContent = new Intl.DateTimeFormat('ru-RU', { month: 'long', year: 'numeric' }).format(new Date(currentYear, currentMonth));
 
 //   for (let i = 1; i <= daysInMonth; i++) {
@@ -81,10 +70,8 @@
 //   }
 // }
 
-// // Initial render
 // renderCalendar();
 
-// // Event listeners for navigation
 // document.getElementById('prevMonth').addEventListener('click', function (e) {
 //   e.preventDefault();
 //   if (currentMonth === 0) {
@@ -114,7 +101,6 @@
 //     'p': 'Тег &lt;p&gt; представляет собой абзац текста.',
 //     'h1': 'Тег &lt;h1&gt; задаёт наибольший заголовок первого уровня.',
 //     'div': 'Тег &lt;div&gt; является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого.',
-//     // Добавьте другие теги и их описания по необходимости
 //   };
 
 //   let  tagInput = document.getElementById('tagInput');
@@ -133,12 +119,9 @@
 //     }
 //   });
 
-
-// ПОИСК ИСТОРИЧЕСКИХ СОБЫТИЙ мб не правильно!!!!!!!!!!!!!!!
 // let events = [
 //     { date: '01.01', name: 'Новый год', description: 'Начало календарного года' },
 //     { date: '07.11', name: 'Октябрьская революция', description: 'Восстание большевиков в 1917 году' },
-//     // Добавьте другие события и их описания по необходимости
 //   ];
 
 //   let input = document.getElementById('input');
@@ -154,15 +137,12 @@
 //   });
 
 //   function renderTable(events) {
-//     // Очищаем предыдущее содержимое таблицы
 //     table.innerHTML = '';
 
-//     // Создаем заголовок таблицы
 //     let headerRow = document.createElement('tr');
 //     headerRow.innerHTML = '<th>Дата</th><th>Название</th><th>Описание</th>';
 //     table.appendChild(headerRow);
 
-//     // Заполняем таблицу событиями
 //     events.forEach(event => {
 //         let row = document.createElement('tr');
 //       row.innerHTML = `<td>${event.date}</td><td>${event.name}</td><td>${event.description}</td>`;
